4217863184a5da21bf6c5c854af183b33afa8594
Ashwin Somasundaram <61034734+AshwinSomasundaram@users.noreply.github.com>
2020-06-30 12:48:29 -0500

    Add files via upload

---
+++ public/js/Snake.js
@@ -0,0 +1,65 @@
+export default class Snake{
+    constructor(scene){
+        this.scene = scene;
+        this.lastMoveTime = 0;
+        this.moveInterval = 250;
+        this.tileSize = 16;
+        this.direction = Phaser.Math.Vector2.LEFT;
+        this.body = [];
+        this.body.push(this.scene.add.rectangle(this.scene.game.config.width/2,this.scene.game.config.width/2,16,16,0x00dd00).setOrigin(0));
+        this.body.push(this.scene.add.rectangle(this.scene.game.config.width/2,this.scene.game.config.width/2,16,16,0x00ff00).setOrigin(0));
+        this.apple = this.scene.add.rectangle(0,0,this.tileSize,this.tileSize,0xff0000).setOrigin(0);
+        this.positionApple();
+        scene.input.keyboard.on('keydown', e =>{this.keydown(e)});
+    }
+
+    positionApple(){
+        this.apple.x = Math.floor(Math.random() * this.scene.game.config.width/this.tileSize)*this.tileSize;
+        this.apple.y=  Math.floor(Math.random() * this.scene.game.config.width/this.tileSize)*this.tileSize;
+    }
+
+    keydown(event){
+        console.log(event);
+        switch(event.keyCode){
+            case 37://Left
+            this.direction = Phaser.Math.Vector2.LEFT;
+                break;
+            case 38://Up
+            this.direction = Phaser.Math.Vector2.UP;
+                break;
+            case 39://Right
+            this.direction = Phaser.Math.Vector2.RIGHT;
+                break;
+            case 40://Down
+            this.direction = Phaser.Math.Vector2.DOWN;
+                break;
+        }
+    }
+    update(time){
+        if(time >= this.lastMoveTime + this.moveInterval){
+            this.lastMoveTime = time;
+            this.move();
+        }
+
+    }
+    move(){
+        let x = this.body[0].x + this.direction.x * this.tileSize;
+        let y = this.body[0].y + this.direction.y * this.tileSize;
+
+        if(this.apple.x === x && this.apple.y === y){
+            this.body.push(this.scene.add.rectangle(0,0,this.tileSize,this.tileSize,0x00ff00).setOrigin(0));
+            this.positionApple();
+        }
+
+        for(let index = this.body.length-1;index>0;index--){
+            this.body[index].x = this.body[index-1].x;
+            this.body[index].y = this.body[index-1].y;
+        }
+        this.body[0].x =x;
+        this.body[0].y =y;
+
+        if(this.body[0].x<0 || this.body[0].x >= this.scene.game.config.width || this.body[0].y < 0 || this.body[0].y>= this.scene.game.config.height){
+            this.scene.scene.restart();
+        }
+    }
+}
